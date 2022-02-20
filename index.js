bf14f41472e96b64227a8f21286a63250186f048
Totolescroc <t_clavel@hetic.eu>
2022-02-20 17:44:21 +0100

    index.css

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ let toggleButton = document.querySelector("#toggle-button");
 let sideBar = document.querySelector("#side-bar");
 let menu = document.querySelector(".menu");
 
+// Menu burger
 toggleButton.addEventListener("click", show);
 
 function show() {
@@ -12,24 +13,24 @@ function show() {
 let js_btn_planning = document.querySelector(".btn_planning");
 let js_btn_shoplist = document.querySelector(".btn_shoplist");
 let js_content_planning = document.querySelector(".content_planning");
-let js_shoplist = document.querySelector(".shoplist");
+let js_content_shoplist = document.querySelector(".content_shoplist");
 
 // Swipe bar planning/liste de courses
 
 // display planning and hide shoplist for the begining
 js_content_planning.style.display = "block";
-js_shoplist.style.display = "none";
+js_content_shoplist.style.display = "none";
 
 // When you click planning button, planning = display and shoplist = hide
 js_btn_planning.addEventListener("click", () => {
-    js_shoplist.style.display = "none";
+    js_content_shoplist.style.display = "none";
     js_content_planning.style.display = "block";
 
 } )
 
 // When you click shoplist button, planning = hide and shoplist = display
 js_btn_shoplist.addEventListener("click", () => {
-    js_shoplist.style.display = "block";
+    js_content_shoplist.style.display = "block";
     js_content_planning.style.display = "none";
 
 } )
@@ -37,7 +38,6 @@ js_btn_shoplist.addEventListener("click", () => {
 function myFunction(x) {
     if (x.matches) { // If media query matches
         js_shoplist.style.display = "block";
-
     } else {
         js_shoplist.style.display = "none";  
     }
